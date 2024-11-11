ab82c4123e3473312c253f6896e8a95d1800675f
Bangarraju-Microsoft <v-golib@microsoft.com>
2024-11-11 17:42:14 +0530

    UI - Jest setup and required packages installed

--- App/frontend-app/jest.config.ts
+++ App/frontend-app/jest.config.ts
@@ -1,33 +1,48 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
-    // Test all files either suffixed with "-test.js", "-test.jsx", "-test.ts", "-test.tsx", or
-    // having ".test.js", ".test.jsx", ".test.ts", ".test.tsx" extensions
-    testRegex: ".*[-.]test\\.(js|ts)x?$",
+import type { Config } from '@jest/types'
 
-    // Generate coverage reports in textm HTML, lcov and clover format
-    coverageReporters: ["text", "html", "lcov", "clover"],
+const config: Config.InitialOptions = {
+  verbose: true,
 
-    // Use the default
-    reporters: ["default"],
+  preset: 'ts-jest',
+  //testEnvironment: 'jsdom',  // For React DOM testing
+  testEnvironment: 'jest-environment-jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['']
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy', // For mocking static file imports
+    '^react-markdown$': '<rootDir>/__mocks__/react-markdown.tsx',
+    '^dompurify$': '<rootDir>/__mocks__/dompurify.js', // Point to the mock
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.ts',
 
-    // Postprocess test result to create a Bamboo format compatible report
-    // testResultsProcessor: "jest-bamboo-reporter",
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'], // For setting up testing environment like jest-dom
+  transform: {
+    '^.+\\.ts(x)?$': 'ts-jest',  // For TypeScript files
+    '^.+\\.js$': 'babel-jest',  // For JavaScript files if you have Babel
+  },
 
-    moduleNameMapper: {
-        // Alias @/ imports
-        "@/(.*)": "<rootDir>/src/$1",
-        // Alias #/ imports
-        "#/(.*)": "<rootDir>/test/$1",
-        // SCSS files
-        "\\.scss$": "identity-obj-proxy",
+  setupFiles: ['<rootDir>/jest.polyfills.js'],
+  collectCoverage: true,
+  //collectCoverageFrom: ['src/**/*.{ts,tsx}'],  // Adjust the path as needed
+  //coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
+  },
 
-    // Project's path which coverage will be reported
-    collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx"],
-    coveragePathIgnorePatterns: [],
-    modulePathIgnorePatterns: [],
-    testPathIgnorePatterns: [],
-};
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/', // Ignore node_modules
+    '<rootDir>/__mocks__/', // Ignore mocks
+    '<rootDir>/src/state/',
+    '<rootDir>/src/api/',
+    '<rootDir>/src/mocks/',
+    //'<rootDir>/src/test/',
+  ],
+}
+
+export default config
