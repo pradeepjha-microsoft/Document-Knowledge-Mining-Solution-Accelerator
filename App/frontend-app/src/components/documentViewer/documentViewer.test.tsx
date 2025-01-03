d6e597eedf9047ad8f0a4531e810fa60dafe040a
Bangarraju-Microsoft <v-golib@microsoft.com>
2025-01-03 19:49:39 +0530

    UI - Unit Test cases for DocumentViewer Component

--- App/frontend-app/src/components/documentViewer/documentViewer.test.tsx
+++ App/frontend-app/src/components/documentViewer/documentViewer.test.tsx
@@ -1,11 +1,16 @@
-
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
 import { DocDialog } from './documentViewer';
+import { MetadataTable } from './metadataTable';
+import { IFrameComponent } from './iFrameComponent';
+import { DialogContentComponent } from './dialogContentComponent';
+import { PagesTab } from './PagesTab';
+import { PageNumberTab } from './pageNumberTab';
+import { AIKnowledgeTab } from './aIKnowledgeTab';
+import { DialogTitleBar } from './dialogTitleBar';
+
 import { Document } from '../../api/apiTypes/embedded';
-// import { useTranslation } from 'react-i18next';
-// import { PagesTab } from './PagesTab';
-import {PostFeedback} from '../../api/chatService';
+import { defaultCalendarStrings } from '@fluentui/react';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
@@ -13,196 +18,245 @@ jest.mock('react-i18next', () => ({
   }),
 }));
 jest.mock("react-tiff", () => ({
-    TIFFViewer: () => <div>Tiff</div>,
+  TIFFViewer: () => <div>Tiff</div>,
 }));
 jest.mock('react-i18next', () => ({
-    useTranslation: () => ({
-      t: (key: any) => key, // Returns the key as the translated string
-    }),
-    // Trans: ({ children }) => children, // Support for <Trans> component
-    initReactI18next: {
-      type: '3rdParty',
-      init: jest.fn(),
-    },
-    i18n: {
-      use: jest.fn(),
-      init: jest.fn(),
-      t: (key: any) => key,
-    },
-  }));
-  
-describe('DocDialog Component', () => {
-  const mockOnClose = jest.fn();
+  useTranslation: () => ({
+    t: (key: any) => key, // Returns the key as the translated string
+  }),
+  // Trans: ({ children }) => children, // Support for <Trans> component
+  initReactI18next: {
+    type: '3rdParty',
+    init: jest.fn(),
+  },
+  i18n: {
+    use: jest.fn(),
+    init: jest.fn(),
+    t: (key: any) => key,
+  },
+}));
 
-  const metadataMock: Document = {
-    document_url : 'http://example.com/document',
+Object.defineProperty(global, "import.meta", {
+  value: {
+    env: {
+      VITE_API_ENDPOINT: "http://mock-api-endpoint.com",
+    },
+  },
+});
+
+
+// Mock Fluent UI Components
+jest.mock('@fluentui/react-components', () => ({
+  ...jest.requireActual("@fluentui/react-components"),
+  Dialog: jest.fn(({ children }) => <div data-testid="Dialog">{children}</div>),
+  DialogSurface: jest.fn(({ children }) => <div data-testid="DialogSurface">{children}</div>),
+  DialogBody: jest.fn(({ children }) => <div data-testid="DialogBody">{children}</div>),
+}));
+
+// Mock External Components
+jest.mock('./metadataTable', () => ({
+  MetadataTable: jest.fn(() => <div data-testid="MetadataTable">MetadataTable</div>),
+}));
+
+jest.mock('./iFrameComponent', () => ({
+  IFrameComponent: jest.fn(() => <div data-testid="IFrameComponent">IFrameComponent</div>),
+}));
+
+jest.mock('./dialogContentComponent', () => ({
+  DialogContentComponent: jest.fn(() => <div data-testid="DialogContentComponent">DialogContentComponent</div>),
+}));
+
+jest.mock('./pageNumberTab', () => ({
+  pageNumberTab: jest.fn(() => <div data-testid="pageNumberTab">pageNumberTab</div>),
+}));
+
+jest.mock('./PagesTab', () => ({
+  PagesTab: jest.fn((props: any) => {
+    const mockMetadata: Document = {
+      document_url: 'http://example.com/document',
+      documentId: '123',
+      fileName: 'test.pdf',
+      page_number: 1,
+      keywords: {
+        field1: "value1",
+        field2: "value2",
+      },
+      importedTime: "string",
+      processingTime: "string",
+      mimeType: "string",
+      summary: "string",
+      id: "string",
+      __partitionkey: "string"
+    };
+    return (<div data-testid="PagesTab">
+      PagesTab
+      <button onClick={props.handlePageClick(mockMetadata)}>PageNumber</button>
+    </div>)
+  }),
+}));
+
+
+
+jest.mock('./aIKnowledgeTab', () => ({
+  AIKnowledgeTab: jest.fn(() => <div data-testid="AIKnowledgeTab">AIKnowledgeTab</div>),
+}));
+
+jest.mock('./dialogTitleBar', () => ({
+  DialogTitleBar: jest.fn((props: any) => <div data-testid="DialogTitleBar">
+    DialogTitleBar
+    <span data-testid="selectedPage" >{props.selectedPage}</span>
+    <span data-testid="selectedTab">{props.selectedTab}</span>
+    <button onClick={(e) => props.onTabSelect(e, { value: 'Pages' })}>PagesTab</button>
+    <button onClick={(e) => props.onTabSelect(e, { value: 'AI Knowledge' })}>AI Knowledge</button>
+    <button onClick={(e) => props.onTabSelect(e, { value: 'Page Number' })}>Page Number</button>
+    <button onClick={(e) => props.onTabSelect(e, { value: 'PageMetadata' })}>PageMetadata</button>
+    <button data-testid="return-to-document-tab" onClick={props.handleReturnToDocumentTab}>Return</button>
+    <button onClick={props.handleDialogClose}>Close</button>
+    <button onClick={props.downloadFile}>Download</button>
+  </div>),
+}));
+
+// Mock API Calls
+jest.mock('../../api/documentsService', () => ({
+  downloadDocument: jest.fn(),
+  getEmbedded: jest.fn(),
+}));
+
+
+
+describe('DocDialog Component', () => {
+
+  beforeEach(() => {
+    // Mock window.open
+    jest.spyOn(window, 'open').mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    // Restore original implementation of window.open
+    jest.restoreAllMocks();
+  });
+
+  const mockMetadata: Document = {
+    document_url: 'http://example.com/document',
     documentId: '123',
     fileName: 'test.pdf',
     page_number: 1,
     keywords: {
-                field1: "value1",
-                field2: "value2",
-            },
+      field1: "value1",
+      field2: "value2",
+    },
     importedTime: "string",
     processingTime: "string",
     mimeType: "string",
     summary: "string",
-    id: "string",                    
+    id: "string",
     __partitionkey: "string"
   };
 
-  Object.defineProperty(global, "import.meta", {
-    value: {
-      env: {
-        VITE_API_ENDPOINT: "http://mock-api-endpoint.com",
-        },
-        },
-    });
   const defaultProps = {
-    metadata: metadataMock,
+    metadata: mockMetadata,
     isOpen: true,
-    allChunkTexts: ['Chunk 1', 'Chunk 2'],
+    allChunkTexts: [],
     clearChatFlag: false,
-    onClose: mockOnClose,
+    onClose: jest.fn(),
   };
-  jest.mock('react-tiff', () => ({
-    TIFFViewer: jest.fn(() => <div data-testid="mock-tiff-viewer">Mocked TIFF Viewer</div>),
-  }));
-  
-jest.mock("./iFrameComponent", () => ({
-    IFrameComponent: () => <div>IFrameComponent</div>,
-}));
 
-jest.mock("./dialogContentComponent", () => ({
-    DialogContentComponent: () => <div>DialogContentComponent</div>,
-}));
-
-jest.mock("./dialogTitleBar", () => ({
-  DialogTitleBar: jest.fn(({ handleReturnToDocumentTab }) => (
-    <div data-testid="dialog-title-bar">
-      <button data-testid="return-to-document-tab" onClick={handleReturnToDocumentTab}>Return</button>
-    </div>
-  )),
-}));
-
-jest.mock("./PagesTab", () => ({
-    PagesTab: () => <div>PagesTab</div>,
-}));
-
-jest.mock("./PageNumberTab", () => ({
-    PageNumberTab: () => <div>PageNumberTab</div>,
-}));
-
-jest.mock("./MetadataTable", () => ({
-    MetadataTable: () => <div>MetadataTable</div>,
-}));
-
-jest.mock("./aIKnowledgeTab", () => ({
-    AIKnowledgeTab: () => <div>AIKnowledgeTab</div>,
-}));
-
-jest.mock("../../api/chatService", () => ({
-  PostFeedback: jest.fn(),
-}));
-
-  it('renders correctly with initial props', () => {
+  it('renders the Dialog and DialogSurface components', () => {
     render(<DocDialog {...defaultProps} />);
-
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
+    expect(screen.getByTestId('Dialog')).toBeInTheDocument();
+    expect(screen.getByTestId('DialogSurface')).toBeInTheDocument();
   });
 
-  it('handles tab selection', () => {
+  it('renders the IFrameComponent when the "Document" tab is selected', () => {
     render(<DocDialog {...defaultProps} />);
-    // Simulate clicking the "Pages" tab
-    const pagesTab = screen.getByText('components.dialog-title-bar.document');
-    fireEvent.click(pagesTab);
-
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
+    expect(screen.getByTestId('IFrameComponent')).toBeInTheDocument();
   });
 
-  it('calls onClose when dialog is closed', () => {
-    render(<DocDialog {...defaultProps} />);
-
-    const closeButton = screen.getByRole('button', { name: /close/i }); // Assuming close button exists in DialogTitleBar
-    fireEvent.click(closeButton);
-
-    expect(mockOnClose).toHaveBeenCalledTimes(1);
+  it('renders the PagesTab when the "Pages" tab is selected', () => {
+    const { rerender } = render(<DocDialog {...defaultProps} />);
+    rerender(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'PagesTab' });
+    fireEvent.click(button);
+    expect(screen.getByTestId('PagesTab')).toBeInTheDocument();
   });
 
-  it('handles download functionality', () => {
-    window.open = jest.fn();
-    render(<DocDialog {...defaultProps} />);
-
-    const downloadButton = screen.getByRole('button', { name: /download/i }); // Assuming download button exists in DialogTitleBar
-    fireEvent.click(downloadButton);
-
-    expect(window.open).toHaveBeenCalledWith(metadataMock.document_url, '_blank');
+  it('renders the AI Knowledge when the "AI Knowledge" tab is selected', () => {
+    const { rerender } = render(<DocDialog {...defaultProps} />);
+    rerender(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'AI Knowledge' });
+    fireEvent.click(button);
+    expect(screen.getByTestId('AIKnowledgeTab')).toBeInTheDocument();
   });
 
-  it('handles iframe key updates on tab changes', () => {
-    render(<DocDialog {...defaultProps} />);
 
+  it('renders the PageMetadata when the "PageMetadata" tab is selected', async () => {
+    const { rerender } = render(<DocDialog {...defaultProps} />);
+    rerender(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'PageMetadata' });
     act(() => {
-      fireEvent.click(screen.getByText('components.dialog-title-bar.document'));
+      fireEvent.click(button);
     });
 
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
-  });
-
-  it('displays AI Knowledge tab correctly', () => {
-    render(<DocDialog {...defaultProps} />);
-
-    fireEvent.click(screen.getByText('components.dialog-title-bar.document'));
+    const selectedTab = screen.getByTestId('selectedTab');
+    expect(selectedTab).toHaveTextContent('PageMetadata');
 
   });
 
-  it('handles page click and updates tab', () => {
-    render(<DocDialog {...defaultProps} />);
 
-    const mockPageMetadata = [{ page_number: 2 }];
+  it('renders the Page Number  when a Page Number Tab is selected and Click on PageNumber', async () => {
+    const { rerender } = render(<DocDialog {...defaultProps} />);
+    rerender(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'PagesTab' });
+    fireEvent.click(button);
+    expect(screen.getByTestId('PagesTab')).toBeInTheDocument();
 
+    const buttonPageNumber = screen.getByRole('button', { name: 'PageNumber' });
     act(() => {
-      fireEvent.click(screen.getByText('components.dialog-title-bar.document'));
-    });
+      fireEvent.click(buttonPageNumber);
+    })
 
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
+    const selectedPage = screen.getByTestId('selectedPage');
+    expect(selectedPage).toHaveTextContent('1');
   });
 
-  it('does not break when metadata is null', () => {
-    render(
-      <DocDialog
-        {...defaultProps}
-        metadata={null}
-      />
-    );
-
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
-  });
-
-  it('updates selected page correctly', () => {
+  it('calls onClose when the dialog close button is clicked', () => {
     render(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'Close' });
+    fireEvent.click(button);
+    expect(defaultProps.onClose).toHaveBeenCalled();
+  });
+
+  it('downloads the document when the download button is clicked', () => {
+    render(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'Download' });
+    fireEvent.click(button);
+    expect(window.open).toHaveBeenCalledTimes(1);
+    expect(window.open).toHaveBeenCalledWith("https://mock-api-endpoint.com/Documents/123/test.pdf", '_blank');
+  });
+
+  it('Retrun to Iframe Component after navigate to other tab', async () => {
+    const { rerender } = render(<DocDialog {...defaultProps} />);
+    rerender(<DocDialog {...defaultProps} />);
+    const button = screen.getByRole('button', { name: 'PagesTab' });
+    fireEvent.click(button);
+    expect(screen.getByTestId('PagesTab')).toBeInTheDocument();
+
+
+    const buttonReturn = screen.getByRole('button', { name: 'Return' });
     act(() => {
-      fireEvent.click(screen.getByText('components.dialog-title-bar.document'));
+      fireEvent.click(buttonReturn);
     });
+    expect(screen.getByTestId('IFrameComponent')).toBeInTheDocument();
 
-    expect(screen.getByText('components.dialog-title-bar.document')).toBeInTheDocument();
   });
 
-  // for un covered lines
-  it("renders dialog when open", () => {
-    render(<DocDialog {...defaultProps} />);
-    expect(screen.getByRole("dialog")).toBeInTheDocument();
-  });
-  /*
-  it("handles return to Document tab and resets state", () => {
-    render(<DocDialog {...defaultProps} />);
 
-    const returnButton = screen.getByTestId("return-to-document-tab");
-    fireEvent.click(returnButton);
-
-    // Verify that the iframe key is incremented
-    // The actual iframe key logic would need a test-id or state verification
-    expect(screen.getByTestId("components.dialog-title-bar.document")).toBeInTheDocument();
+  it('Should not downloads the document when the metaData emtpy or null', () => {
+    const compProps = { ...defaultProps, metadata: null };
+    render(<DocDialog {...compProps} />);
+    const button = screen.getByRole('button', { name: 'Download' });
+    fireEvent.click(button);
+    expect(window.open).toHaveBeenCalledTimes(0);
   });
-  */
+
 });
